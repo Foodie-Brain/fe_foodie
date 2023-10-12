e5396f03c8ef3ec2aaa80b046b945b745db6268f
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-10-12 19:35:31 -0400

    Chore: Render map using Leaflet.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const App = () => {
+
   return (
-    <div className="App">
-      Hey
-    </div>
+    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
   );
 }
 
