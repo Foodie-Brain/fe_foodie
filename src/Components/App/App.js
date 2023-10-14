ebc8a279a9fc187d33b9613a30d0207d3e4d944f
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-10-14 14:37:59 -0400

    Update: Change zoom in map on page load.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,12 +2,11 @@ import './App.css';
 import Map from '../Map/Map';
 
 const App = () => {
-
   return (
     <div>
       <Map />
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,15 +1,62 @@
 import './Map.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 
 const Map = () => {
+  const [position, setPosition] = useState(null)
+
+  function LocationMarker() {
+    const map = useMapEvents({
+      click(e) {
+        console.log(e)
+      const { lat, lng } = e.latlng;
+      console.log(e.latlng)
+      setPosition({ lat, lng });
+      map.flyTo(e.latlng, map.getZoom());
+      }
+    })
+    console.log(position)
+  
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
+
+  // THE CODE BELOW WAS BEING USED TO LOCATE THE USER'S CURRENT LOCATION VIA GEOLOCATION
+  // function LocationMarker() {
+  //   const [position, setPosition] = useState(null)
+  //   const map = useMapEvents({
+  //     click(e) {
+  //       map.locate()
+  //     },
+  //     locationfound(e) {
+  //       setPosition(e.latlng)
+  //       map.flyTo(e.latlng, map.getZoom())
+  //     }
+  //   })
+  //   console.log(position)
+  
+  //   return position === null ? null : (
+  //     <Marker position={position}>
+  //       <Popup>You are here</Popup>
+  //     </Marker>
+  //   )
+  // }
+
   return (
-    <MapContainer center={[39.739235, -104.990250]} zoom={12} scrollWheelZoom={true}>
+    <MapContainer
+      center={{ lat: 39.739235, lng: -104.990250 }}
+      zoom={6}
+    >
       <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <LocationMarker />
     </MapContainer>
-  )
-}
+  );
+};
 
 export default Map;
