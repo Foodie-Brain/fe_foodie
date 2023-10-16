62138b47f3ce50f3f3a9cb8d044d57c187d1c166
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-10-16 16:24:44 -0400

    Feat: Add query to Form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Map from '../Map/Map';
+import Form from '../Form/Form';
 
 const App = () => {
   return (
     <div>
       <Map />
+      <Form />
     </div>
   );
 };
