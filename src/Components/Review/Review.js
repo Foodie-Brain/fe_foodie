84ec9f165e5d3c2b2b4753354a551b95d2c82157
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-10-17 01:38:20 -0400

    Add successful fetch call.

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,39 +1,42 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 
-
 const Review = () => {
-  const [reviewData, setReviewData] = useState(null)
+  const [reviewData, setReviewData] = useState([])
 
   useEffect(() => {
-  fetch('https://be-foodie-brain-b49c609f52cc.herokuapp.com/graphql', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: '{ reviews { name description lat lon } }' }),
-    
-  })
-  .then(res => {console.log('ahhhhh', res)})
-  .then((data) =>  {
-   setReviewData(data)
-})
-}, [])
+    fetch('https://be-foodie-brain-b49c609f52cc.herokuapp.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query: '{ reviews { name description lat lon } }' }), 
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      const reviews = data.data.reviews;
+      setReviewData(reviews);
+      console.log(reviewData)
+    })
+  }, [])
+
   return (
-    <div>
-  {reviewData ? (
-    <div>
-      {reviewData.reviews.map((review, index) => (
-        <div key={index}>
-          <div>Name: {review.name}</div>
-          <div>Description: {review.description}</div>
-          <div>Lat: {review.lat}</div>
-          <div>Lon: {review.lon}</div>
+    <div className='reviews'>
+      {reviewData ? (
+        <div className='existing-reviews'>
+          {reviewData.map((review, index) => (
+            <div key={index}>
+              <div>Name: {review.name}</div>
+              <div>Description: {review.description}</div>
+              <div>Lat: {review.lat}</div>
+              <div>Lon: {review.lon}</div>
+            </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
-  ) : (
-    <div>Loading...</div>
-  )}
-</div>
   )
 }
 
