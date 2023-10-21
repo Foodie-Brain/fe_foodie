e3d83219b1ea1bba6ab4434416ff3b8fabd392c3
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-10-21 19:10:04 -0400

    Test: initial testing for form inputs

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -1,10 +1,22 @@
 describe('shows greeting to user', () => {
   beforeEach(() => {
-    cy.visit('localhost:3000')
+    cy.visit('localhost:3001')
   });
 
 
   it('should show a message on homepage', () => {
     cy.get('.leaflet-container')
   })
+
+  it('should display the Foodie Brain logo', () => {
+    cy.get('.logo').should('be.visible');
+  });
+
+  it('should display a form with Name, Description, Photo Upload, and dietary checkboxes', () => {
+    cy.get('[type="text"]').should('exist');
+    cy.get('.form > [name="description"]').should('exist');
+    cy.get('[type="file"]').should('exist');
+    // cy.get('[data-cy="dietary-checkbox"]').should('exist');
+  });
 })
+
